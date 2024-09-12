4781cdd1b62d029a06a9659dea98bff9db3dffd8
Mark Fasel <markfasel@gmail.com>
2024-09-11 20:42:52 -0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import ProjectSidebar from './components/ProjectSidebar';
+
 function App() {
-  return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
-  );
+    return (
+        <main className="h-screen my-8 flex gap-8">
+            <ProjectSidebar />
+        </main>
+    );
 }
 
 export default App;
