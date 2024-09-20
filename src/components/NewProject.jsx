cf316f94eb8dc62cdd814d67c2c2c03dbdbf7c87
Mark Fasel <markfasel@gmail.com>
2024-09-20 13:20:07 -0400

    class updates and new component

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,27 +1,24 @@
+import Input from './Input';
+
 export default function NewProject() {
     return (
-        <div>
-            <menu>
+        <div className="w-[35rem] mt-16">
+            <menu className="flex items-center justify-end gap-4 my-4">
                 <li>
-                    <button>Cancel</button>
+                    <button className="text-stone-800 hover:text-stone-950">
+                        Cancel
+                    </button>
                 </li>
                 <li>
-                    <button>Save</button>
+                    <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+                        Save
+                    </button>
                 </li>
             </menu>
             <div>
-                <p>
-                    <label>Title</label>
-                    <input />
-                </p>
-                <p>
-                    <label>Description</label>
-                    <textarea></textarea>
-                </p>
-                <p>
-                    <label>Due Date</label>
-                    <input />
-                </p>
+                <Input label="Title" />
+                <Input label="Description" textarea />
+                <Input label="Due Date" />
             </div>
         </div>
     );
