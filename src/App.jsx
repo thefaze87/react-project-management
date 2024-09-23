a260cc78681f74001afdefa3c87f5eb5d30361e9
Mark Fasel <markfasel@gmail.com>
2024-09-23 09:56:22 -0400

    updates to add project list output

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,14 @@ function App() {
 
     function handleAddProject(projectData) {
         setProjectState((prevState) => {
+            const projectId = Math.random();
             const newProject = {
                 ...projectData,
-                id: Math.random(),
+                id: projectId,
             };
             return {
                 ...prevState,
+                selectedProjectId: undefined,
                 projects: [...prevState.projects, newProject],
             };
         });
@@ -45,7 +47,10 @@ function App() {
 
     return (
         <main className="h-screen my-8 flex gap-8">
-            <ProjectSidebar onStartAddProject={handleStartAddProject} />
+            <ProjectSidebar
+                onStartAddProject={handleStartAddProject}
+                projects={projectState.projects}
+            />
             {content}
         </main>
     );
